b5a4d1807fe381b83a5915d88fa691895e1dd956
54570426 <54570426@163.com>
2010-08-30 01:44:32 +0000

    增加相关处理器

---
+++ src/main/java/com/googlecode/asyn4j/exception/Asyn4jException.java
@@ -0,0 +1,30 @@
+/**
+ * Project: asyn4j
+ * 
+ * File Created at 2010-8-27
+ * $Id$
+ * 
+ * Copyright 2008 Alibaba.com Croporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Alibaba Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Alibaba.com.
+ */
+package com.googlecode.asyn4j.exception;
+
+/**
+ * TODO Comment of Asyn4jException
+ * 
+ * @author pan_java
+ * @version Asyn4jException.java 2010-8-27 下午06:55:31
+ */
+public class Asyn4jException extends RuntimeException {
+
+    public Asyn4jException(String message) {
+        super(message);
+    }
+
+}
